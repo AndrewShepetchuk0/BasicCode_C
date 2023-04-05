604a1ca0e2ed2c381e9404f522efb117d657a533
AndrewShepetchuk0 <109487801+AndrewShepetchuk0@users.noreply.github.com>
2023-04-05 19:49:20 +0000

    Create letters_and_numbers.c

---
+++ letters_and_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int main(){
+
+	//Variables
+	int num;
+	char lttr;
+	char *arr[] = {"q","u","i","o"};
+
+	//Generates random number
+	srand(time(NULL));
+	num = rand() % 4;
+
+	//Determines letter based off random number
+	lttr = *arr[num];
+
+	//Prints number and letter
+	printf("%d\n", num);
+	printf("%c\n", lttr);
+
+	return 0;
+}
