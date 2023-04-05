8ba1a9b24df7bd1af1223bb617c00e2437fee078
AndrewShepetchuk0 <109487801+AndrewShepetchuk0@users.noreply.github.com>
2023-04-05 19:50:26 +0000

    Create fizzbuzz.c

---
+++ fizzbuzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+	for(int i = 1; i <= 100; i++){
+		
+		if((i % 3) == 0){
+			printf("%d\t", i);
+			printf("Fizz\n");
+		}
+		if((i % 5) == 0){
+			printf("%d\t", i);
+			printf("Buzz\n");
+		}
+		if(((i % 5) == 0) && ((i % 3) == 0)){
+			printf("%d\t", i);
+			printf("FizzBuzz\n");
+		}
+
+		if((i % 3 != 0) && (i % 5 != 0)){
+			printf("%d\n", i);
+		}
+	}
+}
