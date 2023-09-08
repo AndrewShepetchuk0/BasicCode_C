cf8cadfb5676cbe756b17fcfbe4fe0be023f5287
AndrewShepetchuk0 <109487801+AndrewShepetchuk0@users.noreply.github.com>
2023-09-08 19:46:28 +0000

    Create calculator.c
    Simple calculator with basic operations

---
+++ calculator.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+        float firstnum, secondnum;
+        char operation;
+
+        printf("Enter the first numebr: ");
+        scanf("%f", &firstnum);
+        printf("Enter operation: ");
+        scanf("%c", &operation);
+        printf("Enter the second numebr: ");
+        scanf("%f", &secondnum);
+
+        switch(operation)
+        {
+                case '-':
+                        printf("%f\n", firstnum - secondnum);
+                        break;
+                case '*':
+                        printf("%f\n", firstnum * secondnum);
+                        break;
+                case '+':
+                        printf("%f\n", firstnum + secondnum);
+                        break;
+                case '/':
+                        printf("%f\n", firstnum / secondnum);
+                        break;
+                default:
+                        printf("Not a valid operation\n");
+                }
+        return 0;
+}
