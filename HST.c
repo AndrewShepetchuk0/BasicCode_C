b05ebc2d9fc36a84adee07a682fa1e046cf91b94
AndrewShepetchuk0 <109487801+AndrewShepetchuk0@users.noreply.github.com>
2023-10-29 22:40:12 +0000

    Update HST.c

--- HST.c
+++ HST.c
@@ -7,6 +7,7 @@ int main()
 	//variables for price and tax
 	float price = 0.0;
 	float tax = 1.13;
+	//User input for price
 	printf("Input the price of the item: \n");
 	scanf("%f", &price);
 	printf("The price of the item after HST is: ");
