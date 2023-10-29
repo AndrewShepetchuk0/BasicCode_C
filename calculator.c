6ee6ce335da3c3732a9423428209d8b7cf057a71
AndrewShepetchuk0 <109487801+AndrewShepetchuk0@users.noreply.github.com>
2023-10-29 22:45:34 +0000

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -27,6 +27,7 @@ int main()
                 case '*':
                         printf("%f\n", firstnum * secondnum);
                         break;
+                //Prints result of addition operation
                 case '+':
                         printf("%f\n", firstnum + secondnum);
                         break;
