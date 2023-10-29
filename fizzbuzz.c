41c87e11570704ed3df8a1343b462e2ad9cc11de
AndrewShepetchuk0 <109487801+AndrewShepetchuk0@users.noreply.github.com>
2023-10-29 22:48:57 +0000

    Update fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -10,6 +10,7 @@ int main(){
 			printf("%d\t", i);
 			printf("Fizz\n");
 		}
+		//If the number is divisible by 5, prints Buzz and number
 		if((i % 5) == 0){
 			printf("%d\t", i);
 			printf("Buzz\n");
